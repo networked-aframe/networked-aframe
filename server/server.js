734b6f7f312c1e3c569b2a923bfc109c54c5673d
haydenjameslee <haydenjameslee@gmail.com>
2017-02-09 21:57:25 +1100

    added easyrtc server to server folder in root

---
+++ server/server.js
@@ -0,0 +1,59 @@
+// Load required modules
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var serveStatic = require('serve-static');  // serve static files
+var socketIo = require("socket.io");        // web socket external module
+var easyrtc = require("easyrtc");               // EasyRTC external module
+
+// Set process name
+process.title = "node-easyrtc";
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var app = express();
+app.use(serveStatic('static', {'index': ['index.html']}));
+
+// Start Express http server on port 8080
+var webServer = http.createServer(app).listen(8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = socketIo.listen(webServer, {"log level":1});
+
+easyrtc.setOption("logLevel", "debug");
+
+// Overriding the default easyrtcAuth listener, only so we can directly access its callback
+easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
+        if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+            callback(err, connectionObj);
+            return;
+        }
+
+        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+
+        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+
+        callback(err, connectionObj);
+    });
+});
+
+// To test, lets print the credential to the console for every room join!
+easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
+    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+    easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
+});
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+    console.log("Initiated");
+
+    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+        console.log("roomCreate fired! Trying to create: " + roomName);
+
+        appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
+    });
+});
+
+//listen on port 8080
+webServer.listen(8080, function () {
+    console.log('listening on http://localhost:8080');
+});
---
+++ server/static/index.html
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EasyRTC - Server Root</title>
+    <script type="text/javascript">
+        function forward(){ window.location = "/demos/";}
+    </script>
+</head>
+<body onload="setTimeout('forward()', 10000)">
+    <h1>You have installed EasyRTC!</h1>
+
+    <h2>This is your server's Web Root</h2>
+
+    <p>This is the easiest location to put your own static html files for developing WebRTC applications.</p>
+
+    <p>In the meantime we'll forward you to <a href="/demos/">the EasyRTC Demo page</a>...</p>
+
+    <a href="http://www.easyrtc.com/"><img src="/easyrtc/img/powered_by_easyrtc.png" width="200" height="60" alt="Powered by EasyRTC"></a>
+</body>
+</html>
