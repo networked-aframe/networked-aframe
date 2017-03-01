930d931bf77c0300323528d5dc26ac207fa645fd
haydenjameslee <haydenjameslee@gmail.com>
2017-03-01 22:20:12 +1100

    network update packets are not compressed by default

--- src/NafGlobals.js
+++ src/NafGlobals.js
@@ -4,7 +4,7 @@ var globals = {
   clientId: '',
   debug: false,
   updateRate: 15, // How often network components call `sync`
-  compressSyncPackets: true // compress network component sync packet json
+  compressSyncPackets: false // compress network component sync packet json
 };
 
 module.exports = globals;
