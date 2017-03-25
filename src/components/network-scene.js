d89710d001a42214e301cd881635fac0faf6f67d
haydenjameslee <haydenjameslee@gmail.com>
2017-03-25 19:55:50 +0800

    Changes network-scene schema property: signallingUrl to signalURL and adds deprecation warning

--- src/components/network-scene.js
+++ src/components/network-scene.js
@@ -7,10 +7,13 @@ AFRAME.registerComponent('network-scene', {
     app: {default: 'default'},
     room: {default: 'default'},
     connectOnLoad: {default: true},
-    signallingUrl: {default: '/'},
+    signalURL: {default: '/'},
     audio: {default: false},
     debug: {default: false},
-    onConnect: {default: 'onConnect'}
+    onConnect: {default: 'onConnect'},
+
+    // Deprecated
+    signallingUrl: {default: 'deprecated'}
   },
 
   init: function() {
@@ -28,11 +31,18 @@ AFRAME.registerComponent('network-scene', {
     naf.log.write('Networked-Aframe Connecting...');
 
     // easyrtc.enableDebug(true);
-    var easyRtc = new EasyRtcInterface(easyrtc, this.data.signallingUrl);
+    this.checkDeprecatedProperties();
+    var easyRtc = new EasyRtcInterface(easyrtc, this.data.signalURL);
     naf.connection.setWebRtc(easyRtc);
     if (this.data.onConnect != '' && window.hasOwnProperty(this.data.onConnect)) {
       naf.connection.onLogin(window[this.data.onConnect]);
     }
     naf.connection.connect(this.data.app, this.data.room, this.data.audio);
+  },
+
+  checkDeprecatedProperties: function() {
+    if (this.data.signallingUrl != 'deprecated') {
+      naf.log.error('network-scene `signallingUrl` has been replaced with `signalURL` (uppercase URL)');
+    }
   }
 });
