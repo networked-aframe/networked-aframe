758228a1342095a0c5fc04c12281dedb910dd2d3
machenmusik <machenmusik@gmail.com>
2017-07-14 17:59:27 -0400

    bump dist

--- dist/networked-aframe.js
+++ dist/networked-aframe.js
@@ -58,6 +58,7 @@
 	__webpack_require__(61);
 	__webpack_require__(65);
 	__webpack_require__(66);
+	__webpack_require__(67);
 
 /***/ }),
 /* 1 */
@@ -5115,6 +5116,50 @@
 	    this.unbindRemoteEvents();
 	  }
 	});
+
+/***/ }),
+/* 67 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var naf = __webpack_require__(46);
+
+	AFRAME.registerComponent('networked-adhoc', {
+	  schema: { default: '' },
+	  init: function init() {
+	    addNetEntityFromElement(this.el, this.data);
+	  }
+	});
+
+	function addNetEntityFromElement(el, networkId) {
+	  if (!networkId) {
+	    networkId = Math.random().toString(36).substring(2, 9);
+	  }
+
+	  // make an inline data URI template from the given element
+
+	  el.flushToDOM(); // assume this is synchronous
+	  var n = el.cloneNode(); // make a copy
+	  ['id', 'position', 'rotation', 'networked', 'networked-share', 'networked-remote', 'networked-adhoc',
+	  // 'dynamic-body', 'static-body',
+	  'quaternion', 'velocity'].forEach(function (name) {
+	    n.removeAttribute(name);
+	  });
+
+	  var template = NAF.options.compressSyncPackets ? 'data:text/html;charset=utf-8;base64,' + btoa(n.outerHTML) : 'data:text/html;charset=utf-8,' + encodeURIComponent(n.outerHTML);
+
+	  //el.setAttribute('id', 'naf-' + networkId);
+	  el.setAttribute('networked-share', {
+	    physics: true,
+	    template: template,
+	    showLocalTemplate: false,
+	    networkId: networkId
+	    // this is default now... owner: NAF.clientId // we own it to start
+	  });
+
+	  return el;
+	}
 
 /***/ })
 /******/ ]);
