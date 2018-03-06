62056e60ae9a7917316a3bca9f355d8002539686
Robert Long <robert@robertlong.me>
2018-03-06 13:21:31 -0800

    Fix networked lifecycle.

--- src/components/networked.js
+++ src/components/networked.js
@@ -41,7 +41,7 @@ AFRAME.registerComponent('networked', {
     this.lastOwnerTime = -1;
 
     if (NAF.clientId) {
-      this.onConnected();
+      this.el.addEventListener("loaded", this.onConnected, false);
     } else {
       document.body.addEventListener('connected', this.onConnected, false);
     }
@@ -119,7 +119,10 @@ AFRAME.registerComponent('networked', {
     if (this.data.owner === '') {
       this.lastOwnerTime = NAF.connection.getServerTime();
       this.el.setAttribute(this.name, {owner: NAF.clientId});
-      this.syncAll();
+
+      setTimeout(() => {
+        this.syncAll();
+      }, 0);
     }
 
     document.body.removeEventListener('connected', this.onConnected, false);
