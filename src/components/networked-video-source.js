61ba18eee225dcb999c2ed6311985fe681ca0b81
Harold Thetiot <hthetiot@gmail.com>
2021-03-08 02:43:36 +0100

    implement networked-video-source components and basic-video examples

---
+++ src/components/networked-video-source.js
@@ -0,0 +1,97 @@
+/* global AFRAME, NAF, THREE */
+var naf = require('../NafIndex');
+
+// https://stackoverflow.com/questions/53905525/aframe-how-to-render-a-camera-to-a-texture
+
+AFRAME.registerComponent('networked-video-source', {
+
+  schema: {
+  },
+
+  dependencies: ['material'],
+
+  init: function () {
+    this.videoTexture = null;
+    this.video = null;
+    this.stream = null;
+
+    this._setMediaStream = this._setMediaStream.bind(this);
+
+    NAF.utils.getNetworkedEntity(this.el).then((networkedEl) => {
+      const ownerId = networkedEl.components.networked.data.owner;
+
+      if (ownerId) {
+        NAF.connection.adapter.getMediaStream(ownerId, "video")
+          .then(this._setMediaStream)
+          .catch((e) => naf.log.error(`Error getting media stream for ${ownerId}`, e));
+      } else {
+        // Correctly configured local entity, perhaps do something here for enabling debug audio loopback
+      }
+    });
+  },
+
+  update() {
+
+  },
+
+  _setMediaStream(newStream) {
+
+    if(!this.video) {
+      this.setupVideo();
+    }
+
+    if(newStream != this.stream) {
+      if (this.stream) {
+        this._clearMediaStream();
+      }
+
+      if (newStream) {
+        this.video.srcObject = newStream;
+
+        var playResult = this.video.play();
+        if (playResult instanceof Promise) {
+          playResult.catch((e) => naf.log.error(`Error play video stream`, e));
+        }
+
+        const mesh = this.el.getObject3D('mesh');
+        mesh.material.map = new THREE.VideoTexture(this.video);
+        mesh.material.needsUpdate = true;
+      }
+
+      this.stream = newStream;
+    }
+  },
+
+  _clearMediaStream() {
+    if (this.video) {
+      this.video.srcObject = null;
+      this.video = null;
+      this.stream = null;
+    }
+  },
+
+  remove: function() {
+    if (!this.videoTexture) return;
+
+    if (this.stream) {
+        this._clearMediaStream();
+    }
+  },
+
+  setupVideo: function() {
+    var el = this.el;
+
+    if (this.video) {
+      el.removeObject3D(this.attrName);
+    }
+
+    if (!this.video) {
+      var video = document.createElement('video');
+      video.setAttribute('autoplay', true);
+      video.setAttribute('playsinline', true);
+      video.setAttribute('muted', true);
+    }
+
+    this.video = video;
+  }
+});
