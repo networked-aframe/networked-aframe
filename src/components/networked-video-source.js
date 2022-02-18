3be52f447b0ed21c7a40d2490d989553029f312c
Dimitrios Ververidis <jimver04@gmail.com>
2022-02-18 09:52:32 +0200

    Add to networked-video-source.js component the ability to do the green screen effect. - Global Threshold for all clients through a shader - No multistreams, just adaptations on the main stream - Changed init to update in networked-video-source.js in order to be able to change GreenThreshold with dat.gui

--- src/components/networked-video-source.js
+++ src/components/networked-video-source.js
@@ -6,7 +6,11 @@ AFRAME.registerComponent('networked-video-source', {
   schema: {
     streamName: { default: 'video' },
     useGreenScreen: {default: false},
-    greenThreshold : {default: 0.02}
+    ThresholdMin : {default: 0.106},
+    ThresholdMax : {default: 0.13},
+    red: {default: 48},
+    green: {default: 146},
+    blue: {default: 89}
   },
 
   dependencies: ['material'],
@@ -63,7 +67,11 @@ AFRAME.registerComponent('networked-video-source', {
           this.uniforms = {};
 
           this.uniforms.uMap = {type: 't', value: this.videoTexture}
-          this.uniforms.greenThreshold = {type: 'float', value: this.data.greenThreshold};
+          this.uniforms.ThresholdMin = {type: 'float', value: this.data.ThresholdMin};
+          this.uniforms.ThresholdMax = {type: 'float', value: this.data.ThresholdMax};
+          this.uniforms.red = {type: 'float', value: this.data.red};
+          this.uniforms.green = {type: 'float', value: this.data.green};
+          this.uniforms.blue = {type: 'float', value: this.data.blue};
 
           this.uniforms = THREE.UniformsUtils.merge([
             this.uniforms,
@@ -75,7 +83,7 @@ AFRAME.registerComponent('networked-video-source', {
           });
 
           this.materialIncoming.vertexShader = `
-                         varying vec2 vUv;
+                        varying vec2 vUv;
 
                         void main() {
                             vec4 worldPosition = modelViewMatrix * vec4( position, 1.0 );
@@ -88,15 +96,45 @@ AFRAME.registerComponent('networked-video-source', {
           this.materialIncoming.fragmentShader = `
                            varying vec2 vUv;
                            uniform sampler2D uMap;
-                           uniform float greenThreshold;
+                           uniform float ThresholdMin;
+                           uniform float ThresholdMax;
+                           uniform float red;
+                           uniform float green;
+                           uniform float blue;
                            
-                           void main() {
+                           mat4 RGBtoYUV = mat4(0.257,  0.439, -0.148, 0.0,
+                                                 0.504, -0.368, -0.291, 0.0,
+                                                 0.098, -0.071,  0.439, 0.0,
+                                                 0.0625, 0.500,  0.500, 1.0 );
+                           
+                          void main() {
                                 vec2 uv = vUv;
                                 vec4 tex1 = texture2D(uMap, uv * 1.0);
-                                 if (tex1.g - tex1.r > greenThreshold)
-                                    discard; 
-                                 else
-                                    gl_FragColor = vec4(tex1.r,tex1.g,tex1.b,1.0);
+
+                                //color to be removed
+                                vec4 chromaKey = vec4(red / 255.0, green/255.0, blue/255.0, 1);
+                                        
+                                //convert from RGB to YCvCr/YUV
+                                vec4 keyYUV =  RGBtoYUV * chromaKey;
+                                
+                                vec4 yuv = RGBtoYUV * tex1;
+                                
+                                float cc;
+                                
+                                float tmp = sqrt(pow(keyYUV.g - yuv.g, 2.0) + pow(keyYUV.b - yuv.b, 2.0));
+                                if (tmp < ThresholdMin)
+                                  cc = 0.0;
+                                else if (tmp < ThresholdMax)
+                                   cc = (tmp - ThresholdMin)/(ThresholdMax - ThresholdMin);
+                                else
+                                   cc=  1.0;
+                                
+                                gl_FragColor = max(tex1 - (1.0 - cc) * chromaKey, 0.0);
+                                                                
+                                 // if (tex1.g - tex1.r > greenThreshold)
+                                 //    discard; 
+                                 // else
+                                 //    gl_FragColor = fragColor;
                             }
                       `;
 
