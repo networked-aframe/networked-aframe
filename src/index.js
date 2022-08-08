096259afd77571f75528e857b14f834b9a3270a5
kyle <me@kylebaker.io>
2022-08-07 20:16:30 -0600

    networked-hand-controllers first pull-request draft

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ require('./components/networked-scene');
 require('./components/networked');
 require('./components/networked-audio-source');
 require('./components/networked-video-source');
+require('./components/networked-hand-controls');
