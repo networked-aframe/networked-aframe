0f6330efcd345447299381e9b53484978fde1690
Vincent Fretin <vincent.fretin@gmail.com>
2023-07-12 16:39:42 +0200

    Bump version to 0.11.2

--- src/NafIndex.js
+++ src/NafIndex.js
@@ -14,7 +14,7 @@ naf.options = options;
 naf.utils = utils;
 naf.log = new NafLogger();
 naf.schemas = new Schemas();
-naf.version = "0.11.1";
+naf.version = "0.11.2";
 
 naf.adapters = new AdapterFactory();
 var entities = new NetworkEntities();
